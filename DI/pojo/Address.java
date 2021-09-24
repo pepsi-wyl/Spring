3e12161f5861ba8fa909742734f7213ff7a0b820
wyl <82953346+pepsi-wyl@users.noreply.github.com>
2021-09-24 21:17:20 +0800

    Create Address.java

---
+++ DI/pojo/Address.java
@@ -0,0 +1,20 @@
+package pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author by wyl
+ * @date 2021/9/24.19点49分
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+
+public class Address {
+    private String address;
+}
