9af99b585c675f38fb4f95fa37392b4695064831
wyl <82953346+pepsi-wyl@users.noreply.github.com>
2021-09-25 15:40:05 +0800

    Create ApplicationContextUtils.java

---
+++ annotation/ApplicationContextUtils.java
@@ -0,0 +1,24 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author by wyl
+ * @date 2021/9/24.19点46分
+ */
+
+public class ApplicationContextUtils {
+
+    /**
+     * xml方式获取Spring配置文件
+     */
+    //默认
+    public static ApplicationContext getApplicationContext() {
+        return new ClassPathXmlApplicationContext("applicationContext.xml");
+    }
+
+    //自定义
+    public static ApplicationContext getApplicationContext(String xmlName) {
+        return new ClassPathXmlApplicationContext(xmlName + ".xml");
+    }
+
+}
