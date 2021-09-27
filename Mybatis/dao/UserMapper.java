a9f040e23a38d927e321c2c6f2a2c3d9390049d1
wyl <82953346+pepsi-wyl@users.noreply.github.com>
2021-09-27 20:28:37 +0800

    Create UserMapper.java

---
+++ Mybatis/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package dao;
+
+import java.util.List;
+
+/**
+ * @author by wyl
+ * @date 2021/9/26.14点59分
+ */
+public interface UserMapper {
+
+    /**
+     * 查找所有用户
+     */
+    List getUserList();
+
+
+}
