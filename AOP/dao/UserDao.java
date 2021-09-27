efa7ccfe65b0ab0c0fa99d557befd94d5fad78b9
wyl <82953346+pepsi-wyl@users.noreply.github.com>
2021-09-27 20:18:42 +0800

    Create UserDao.java

---
+++ AOP/dao/UserDao.java
@@ -0,0 +1,18 @@
+package dao;
+
+/**
+ * @author by wyl
+ * @date 2021/9/25.21点05分 接口
+ */
+
+public interface UserDao {
+
+    void add();
+
+    void delete();
+
+    void query();
+
+    void modify();
+
+}
