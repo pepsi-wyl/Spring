0c07e1ca94b50132177ba088500455f6361b911d
wyl <82953346+pepsi-wyl@users.noreply.github.com>
2021-10-03 11:40:06 +0800

    Update UserMapperImpl2.java

--- Mybatis/dao/UserMapperImpl2.java
+++ Mybatis/dao/UserMapperImpl2.java
@@ -9,6 +9,12 @@ import java.util.List;
  * @date 2021/9/26.19点15分
  */
 
+/**
+    <bean id="userMapper2" class="dao.UserMapperImpl2">
+        <property name="sqlSessionFactory" ref="sqlSessionFactory"></property>
+    </bean> 
+ */
+
 public class UserMapperImpl2 extends SqlSessionDaoSupport implements UserMapper {
 
     @Override
