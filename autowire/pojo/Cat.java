976969c9d6ddd01dd2ddd313b1287d8a944c9aca
wyl <82953346+pepsi-wyl@users.noreply.github.com>
2021-11-08 14:47:04 +0800

    Create Cat.java

---
+++ autowire/pojo/Cat.java
@@ -0,0 +1,17 @@
+package pojo;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author by wyl
+ * @date 2021/9/25.09点53分
+ */
+
+@Component
+public class Cat {
+
+    public void fun() {
+        System.out.println("miao~");
+    }
+
+}
