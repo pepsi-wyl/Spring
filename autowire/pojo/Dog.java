5e8ae8a433b806d9e0b35565da27409546fed67a
wyl <82953346+pepsi-wyl@users.noreply.github.com>
2021-11-08 14:47:22 +0800

    Create Dog.java

---
+++ autowire/pojo/Dog.java
@@ -0,0 +1,17 @@
+package pojo;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author by wyl
+ * @date 2021/9/25.09点52分
+ */
+
+@Component
+public class Dog {
+
+    public void fun(){
+        System.out.println("wang~");
+    }
+
+}
