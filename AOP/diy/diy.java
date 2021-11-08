63760326b20d8b9f30c2c029428b28cee5b4cddb
wyl <82953346+pepsi-wyl@users.noreply.github.com>
2021-11-08 15:00:57 +0800

    Update diy.java

--- AOP/diy/diy.java
+++ AOP/diy/diy.java
@@ -1,17 +1,21 @@
 package diy;
+package diy;
 
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.context.annotation.Scope;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
 
 /**
  * @author by wyl
  * @date 2021/9/26.
+ */
+
+/**
+ * 执行顺序   环绕前  前置  方法体  afterReturning/afterThrowing  after  环绕后
  */
 
 @Component             //托管到Spring中
@@ -19,25 +23,56 @@ import org.springframework.stereotype.Component;
 
 @Aspect                //标注此类是切面类
 
+@Order(1)              //@Order 设置该类对方法增强的优先级,数字越小优先级越高
 public class diy {
 
     /**
-     * 前置
+     * @Pointcut (execution表达式) [公共切点]
+     * 抽取公共的切入点
      */
-    @Before("execution(* dao.UserDaoImpl.*(..))")
+    @Pointcut("execution(* dao.UserDaoImpl.*(..))")
+    public void pointcutPublic() {
+    }
+
+
+    /**
+     * 前置通知
+     */
+    @Before("pointcutPublic()")          //引用公共切入点
     public void before() {
         System.out.println("before!");
     }
 
+    /**
+     * 异常通知
+     */
+    @AfterThrowing("execution(* dao.UserDaoImpl.*(..))")
+    public void afterThrowing() {
+        System.out.println("afterThrowing!");
+    }
+
+    /**
+     * 后置通知（返回通知）  异常出现不执行
+     */
+    @AfterReturning("execution(* dao.UserDaoImpl.*(..))")
+    public void afterReturning() {
+        System.out.println("afterReturning!");
+    }
+
+    /**
+     * 最终通知      异常出现也执行
+     */
     @After("execution(* dao.UserDaoImpl.*(..))")
     public void after() {
         System.out.println("after!");
     }
 
+    /**
+     * 环绕通知
+     */
     @Around("execution(* dao.UserDaoImpl.*(..))")
     public void around(ProceedingJoinPoint point) throws Throwable {
         System.out.println("执行前");
-//        System.out.println("signature" + point.getSignature());//获得签名
         point.proceed();    //相当于过滤
         System.out.println("执行后");
     }
