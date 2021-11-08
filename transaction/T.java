c22447c4452707a722962e5250c421c879ab842d
wyl <82953346+pepsi-wyl@users.noreply.github.com>
2021-11-08 15:17:47 +0800

    Update T.java

--- transaction/T.java
+++ transaction/T.java
@@ -1,8 +1,19 @@
-    
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import service.UserService;
+import utils.ApplicationContextUtils;
+
+/**
+ * @author by wyl
+ * @date 2021/9/26.
+ */
+public class Transactional_T {
 
     @Test
-    public void t4(){
+    public void test(){
         ApplicationContext context = ApplicationContextUtils.getApplicationContext();
         UserService userService = context.getBean("userService", UserService.class);
         userService.add_del();
     }
+
+}
