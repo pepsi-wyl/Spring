4e5049b3ff6edac20e3e957bc44f8e6eae38506e
wyl <82953346+pepsi-wyl@users.noreply.github.com>
2021-11-08 15:01:43 +0800

    Update UserDao.java

--- AOP/dao/UserDao.java
+++ AOP/dao/UserDao.java
@@ -1,18 +1,15 @@
 package dao;
 
+
 /**
  * @author by wyl
  * @date 2021/9/25.21点05分 接口
  */
 
+
 public interface UserDao {
-
+    /**
+     * 测试方法
+     */
     void add();
-
-    void delete();
-
-    void query();
-
-    void modify();
-
 }
